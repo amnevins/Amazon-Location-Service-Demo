574a452c8739b3b6e64ae76b861685113e992f5b
Nevins <amnevins@gmail.com>
2024-08-25 13:56:18 -0700

    NodeJS12 update to Node18

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awslocationtemplate808dd50a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
